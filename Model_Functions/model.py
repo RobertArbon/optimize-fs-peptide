d61eb848d98e6dedf86cb871eee0ce7abf8b6254
Robert Arbon <robert.arbon@gmail.com>
2017-06-15 20:44:22 +0100

    Started building Osprey Model Function

--- Model_Functions/model.py
+++ Model_Functions/model.py
@@ -14,15 +14,16 @@ from six import iteritems
 # The data will be loaded with a stride of 10 frames.  Each fame is 50ps, so the time per frame will be
 # 500ps/frame or 0.5ns/frame.
 # Each trajectory is 1000 frames long
-# Lag time will be 40 (20 ns) frames based on a visual inspection of /Misc/MSM_lag_time.ipynb
+# Lag time will be 40 frames (20 ns)  based on a visual inspection of /Misc/MSM_lag_time.ipynb
 to_ns = 0.5
 msm_lag = int(40/to_ns)
 
-# [‘phi’, ‘psi’, ‘omega’, ‘chi1’, ‘chi2’, ‘chi3’, ‘chi4’]
+# Put all the features here but only select one at a time
 
-feats = [('backbone_dihed', DihedralFeaturizer(types=['phi', 'psi'])),
-        ('residues_dihed', DihedralFeaturizer(types=['chi1', 'chi2', 'chi3', 'chi4'])),
-        ('contacts', ContactFeaturizer())]
+feats = [('bb1_dihed', DihedralFeaturizer(types=['phi', 'psi'])),
+         ('bb2_dihed', DihedralFeaturizer(types=['phi', 'psi', 'omega'])),
+         ('res_dihed', DihedralFeaturizer(types=['chi1', 'chi2', 'chi3', 'chi4'])),
+         ('contacts', ContactFeaturizer())]
 
 featurizer = FeatureSelector(features=feats)
 
