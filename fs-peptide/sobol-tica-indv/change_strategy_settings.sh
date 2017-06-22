73b8f1ffb44db19b0c3c416f63c5b835030a13db
Robert Arbon <robert.arbon@gmail.com>
2017-06-22 18:13:36 +0100

    Changed config files to fix erroneous deletions

--- fs-peptide/sobol-tica-indv/change_strategy_settings.sh
+++ fs-peptide/sobol-tica-indv/change_strategy_settings.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 
+#for f in `ls *.yaml`
+#do
+#    W=${f%.*}
+#    cp $f $W
+#    sed -i '.original' 's/name\: hyperopt_tpe/name\: sobol/' $f
+#    sed -i '.original' '/params:/d' $f
+#    sed -i '.original' '/seeds:/d' $f
+#    sed -i '.original' '/gamma:/d' $f
+#
+#done
+
 for f in `ls *.yaml`
 do
-    W=${f%.*}
-    cp $f $W
-    sed -i '.original' 's/name\: hyperopt_tpe/name\: sobol/' $f
-    sed -i '.original' '/params:/d' $f
-    sed -i '.original' '/seeds:/d' $f
-    sed -i '.original' '/gamma:/d' $f
+    sed -i '.original' 's/    name\: shufflesplit/    name: shufflesplit\
+    params\:/' $f
+    sed -i '.original' 's/  name\: mdtraj/  name\: mdtraj\
+  params\:/' $f
 
 done
