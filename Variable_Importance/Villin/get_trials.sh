563e57e2477472cd99a7b80c9d199f2bf94fc96b
Robert Arbon <robert.arbon@gmail.com>
2017-07-21 12:30:28 +0100

    Started to Variable Importance Analysis

---
+++ Variable_Importance/Villin/get_trials.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Random/osprey-trails.db Random/
+scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Random/alpha_angle.yaml Random/config.yaml
+
+scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Sobol/osprey-trails.db Sobol/
+scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Sobol/alpha_angle.yaml Sobol/config.yaml
