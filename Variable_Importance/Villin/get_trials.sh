9a203dec3bb3c8c8bebf204589e5f2c63b5d4224
Robert Arbon <robert.arbon@gmail.com>
2017-07-21 17:46:02 +0100

    Most of variable importance notebook finished.

--- Variable_Importance/Villin/get_trials.sh
+++ Variable_Importance/Villin/get_trials.sh
@@ -2,7 +2,7 @@
 
 
 scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Random/osprey-trails.db Random/
-scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Random/alpha_angle.yaml Random/config.yaml
+scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Random/\*.yaml Random/
 
 scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Sobol/osprey-trails.db Sobol/
-scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Sobol/alpha_angle.yaml Sobol/config.yaml
+scp ra15808@bluecrystalp3.bris.ac.uk:~/hyperparamopt/Villin/Sobol/\*.yaml Sobol/
