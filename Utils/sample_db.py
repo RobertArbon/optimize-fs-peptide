2ed4b913235f17d5bec964b40fca79ad59692b5c
Robert Arbon <robert.arbon@gmail.com>
2017-07-28 10:18:48 +0100

    Formatting changes

--- Utils/sample_db.py
+++ Utils/sample_db.py
@@ -17,14 +17,14 @@ hist1 = config1.trials().query(Trial).all()
 # Main loop
 for name, group in df1.groupby('project_name'):
     # Sample the group
-    sample = group.sample(int(num))
+    sample = group.sample(int(num), random_state=42)
     keep = sample['id'].values
 
     db2 = make_session('sqlite:///osprey-trials-{}.db'.format(num), project_name=name)
 
     # Get the relevant trial objects from original db
     save_trials = [t for t in hist1 if t.project_name == name and t.id in keep and t.status == 'SUCCEEDED']
-    print('Project name {0} \n Adding {1} trials'.format(name, len(save_trials)))
+    print('Project name {0} Adding {1} trials {2}'.format(name, len(save_trials), keep))
 
     # merge them into new db
     for trial in save_trials:
