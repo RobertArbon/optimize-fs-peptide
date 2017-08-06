c11e4080b5634afff3f64097c878c33fcab493ab
Robert <robert.arbon@gmail.com>
2017-08-06 15:06:11 +0100

    Added test workbook for run from trials

--- DHFR/Random-GMRQ-CSE/run_from_trials.py
+++ DHFR/Random-GMRQ-CSE/run_from_trials.py
@@ -17,11 +17,19 @@ config_path = '../../Trial Data/DHFR/Random-GMRQ-2/alpha_angle.yaml'
 db_path = '../../Trial Data/DHFR/Random-GMRQ-2/osprey-trials.db'
 traj_dir = '/home/robert/Datasets/DHFR/train'
 
-# the pipeline to use
-pipe = Pipeline([('variance_cut', VarianceThreshold()),
-          ('tica', tICA(kinetic_mapping=True)),
-          ('cluster', MiniBatchKMeans()),
-          ('msm', MarkovStateModel(use_gap='timescales', lag_time=50, verbose=True))])
+
+def get_pipeline(parameters):
+    """
+    Wrapper so that new instance of a pipeline can be instantiated for every fold. 
+    :return: sklean.pipeline.Pipeline object
+    """
+    pipe = Pipeline([('variance_cut', VarianceThreshold()),
+                     ('tica', tICA(kinetic_mapping=True)),
+                     ('cluster', MiniBatchKMeans()),
+                     ('msm', MarkovStateModel(use_gap='timescales', lag_time=50, verbose=True))])
+    pipe.set_params(**parameters)
+
+    return pipe
 
 
 # cross validation iterator
@@ -34,7 +42,7 @@ pipe = Pipeline([('variance_cut', VarianceThreshold()),
 cv = ShuffleSplit(n_splits=5, test_size=0.5)
 
 
-def get_trajectoris(feat):
+def get_trajectories(feat):
     """
     Gets the trajctories associated with a feature
     :param feat: 
@@ -71,6 +79,9 @@ if __name__ == "__main__":
     np.random.seed(42)
     config = Config(config_path)
     trials = config.trial_results()
+    trials = trials.iloc[:5,:]
+    print('original df: ')
+    print(trials.head())
 
     ## Do it in parallel
     # with Pool() as pool:
@@ -78,12 +89,12 @@ if __name__ == "__main__":
     new_trial_params = [get_parameters(irow) for irow in trials.iterrows()]
 
     trial_config = new_trial_params[0]
+
     feat = trial_config['feature']
-    X = get_trajectoris(feat)
+    X = get_trajectories(feat)
 
     row_num = trial_config['row']
     params = trial_config['params']
-    pipe.set_params(**params)
 
     train_timescales = []
     test_timescales = []
@@ -92,12 +103,23 @@ if __name__ == "__main__":
     train_scores = []
     test_scores = []
 
-    for train, test in cv.split(X):
-        print(X)
-        print(train)
-        # pipe.fit(X[train])
-        # train_scores.append(pipe.score(X[train]))
+    for train_idx, test_idx in cv.split(X):
+        # the pipeline to use
+        pipe = get_pipeline(params)
 
+        train = [X[i] for i in train_idx]
+        test = [X[i] for i in test_idx]
+        pipe.fit(train)
+        train_timescales.append(pipe.named_steps['msm'].n_timescales)
+        train_gaps.append(pipe.named_steps['msm'].gap_)
+        train_scores.append(pipe.score(train))
+
+        # train_scores.append(pipe.score(X[train]))
+    print(train_timescales)
+    print(train_gaps)
+    print(train_scores)
+
+    # trials.ix[row_num,'cse_n_timescales'] =
 
 
 
