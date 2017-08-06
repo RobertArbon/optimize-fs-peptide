81693fa9fba0c874290bc5efcf5a947c3d461a8f
Robert Arbon <ra15808@bc4login2.bc4.acrc.priv>
2017-08-06 16:56:59 +0100

    Added submit files for running on BCp4

--- DHFR/AdaptiveSampling/submit-alpha_angle.sh
+++ DHFR/AdaptiveSampling/submit-alpha_angle.sh
@@ -2,11 +2,11 @@
 #SBATCH --time=2:00:00
 #SBATCH --mem=4000
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=16
 
 NO_OF_CORES=$(expr $SLURM_TASKS_PER_NODE \* $SLURM_JOB_NUM_NODES)
 
 for i in `seq $NO_OF_CORES`; do
-    srun -n 1 osprey worker alpha_angle.yaml -n 25 --seed $i > osprey.$SLURM_JOB_ID.$i.log 2>&1 &
+    srun -n 1 osprey worker alpha_angle.yaml -n 13 --seed $i > osprey.$SLURM_JOB_ID.$i.log 2>&1 &
 done
 wait
