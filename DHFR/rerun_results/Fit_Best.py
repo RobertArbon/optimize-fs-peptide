30a58bd1868a89b8b7bca0c55fed88b98837fb29
Robert Arbon <robert.arbon@gmail.com>
2017-08-22 13:17:04 +0100

    Added method to save results.

--- DHFR/rerun_results/Fit_Best.py
+++ DHFR/rerun_results/Fit_Best.py
@@ -16,11 +16,9 @@ from sklearn.model_selection import cross_val_score, cross_val_predict
 
 
 # Globals
-# traj_dir = '/mnt/storage/home/ra15808/scratch/train'
-traj_dir = '/Users/robert_arbon/Datasets/DHFR/train'
-
-cv = ShuffleSplit(n_splits=2, test_size=0.5)
-
+num_procs = 14
+traj_dir = '/mnt/storage/home/ra15808/scratch/train'
+# traj_dir = '/Users/robert_arbon/Datasets/DHFR/train'
 
 pipe_fixed = Pipeline([
             ('variance_cut', VarianceThreshold()),
@@ -38,6 +36,7 @@ best = pd.read_pickle('best_trials.pickl')
 
 best.sort_values(by='rank', inplace=True)
 
+results = {'id': [], 'new_test_scores': [], 'strategy': []}
 for i, row in best.iterrows():
     # Choose pipeline
     if row['strategy'] == 'fixed':
@@ -56,8 +55,18 @@ for i, row in best.iterrows():
 
     # Set params
     params = row.filter(regex='.*__.*').to_dict()
-    print(params)
     pipe.set_params(**params)
 
-    test_scores = cross_val_score(pipe, trajs, cv=cv, n_jobs=2, verbose=1)
-    print(test_scores)
+    # Get CV metrics
+    cv = ShuffleSplit(n_splits=50, test_size=0.5)
+
+    test_scores = cross_val_score(pipe, trajs, cv=cv, n_jobs=num_procs, pre_dispatch=num_procs, verbose=1)
+
+    results['id'].append(row['id'])
+    results['new_test_scores'].append(test_scores)
+    results['strategy'].append(row['strategy'])
+
+new_results = pd.DataFrame(data=results)
+new_results = new_results.merge(right=best, on=['id','strategy'], how='inner')
+
+new_results.to_pickle('best_trials_50foldCV.pickl')
