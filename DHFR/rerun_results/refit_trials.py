45eacb2981f060b93abd146d2c5a80e342cf43f3
Robert Arbon <robert.arbon@gmail.com>
2017-08-24 09:17:47 +0100

    Changed refitting scripts so that they can be run independently.

--- DHFR/rerun_results/refit_trials.py
+++ DHFR/rerun_results/refit_trials.py
@@ -12,19 +12,14 @@ from sklearn.model_selection import cross_val_score
 import sys
 
 """
-Rescores trials with user supplied number of timescales.
+Refits trials with all the data and records the timescales.
 """
 
-new_n_timescales = int(sys.argv[1])
-if new_n_timescales is None:
-    sys.exit(1)
-
 # Globals
-num_procs = 5 # Should pick this up from Slurm E-V.
 traj_dir = '/mnt/storage/home/ra15808/scratch/train'
 # traj_dir = '/Users/robert_arbon/Datasets/DHFR/train'
 trial_db = 'best_trials.pickl'
-output_db = trial_db.split('.')[0]+'-'+str(new_n_timescales)+'.pickl'
+output_db = trial_db.split('.')[0]+'-'+'all_ts'+'.pickl'
 
 # Pipelines
 pipe = Pipeline([
@@ -38,13 +33,13 @@ best = pd.read_pickle(trial_db)
 best.sort_values(by='feature', inplace=True)
 
 # Setup results dictionary
-results = {'id': [], 'strategy': [], 'test_scores-{}'.format(new_n_timescales): []}
+results = {'id': [], 'strategy': [], 'all_timescales': []}
 
 # Loop
-cv = ShuffleSplit(n_splits=5, test_size=0.5, random_state=42)
 old_feature = 'none'
 for i, row in best.iterrows():
     print('---Running {}---'.format(i))
+
     # Get dataset
     if row['feature'] != old_feature:
         traj_paths = glob(join(traj_dir, row['feature'], '*.npy'))
@@ -59,17 +54,13 @@ for i, row in best.iterrows():
     # Set params
     params = row.filter(regex='.*__.*').to_dict()
     pipe.set_params(**params)
+    pipe.set_params(msm__n_timescales=None)
+    pipe.fit(trajs)
 
     results['id'].append(row['id'])
     results['strategy'].append(row['strategy'])
-
-    pipe.set_params(msm__n_timescales=new_n_timescales)
-    try:
-        test_scores = cross_val_score(pipe, trajs, cv=cv, n_jobs=num_procs, pre_dispatch=num_procs, verbose=1)
-    except:
-        test_scores = [None]
-
-    results['test_scores-{}'.format(new_n_timescales)].append(test_scores)
+    all_ts = pipe.named_steps['msm'].timescales_
+    results['all_timescales'].append(all_ts)
 
 # Save results
 new_results = pd.DataFrame(data=results)
